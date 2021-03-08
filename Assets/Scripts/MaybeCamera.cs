44965d07c094e55ec87e0f2d37ff43f94f58c5bf
PAVUK <ZetoX3429@mail.ru>
2021-03-09 01:00:55 +0300

    Initial commit

---
+++ Assets/Scripts/MaybeCamera.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MaybeCamera : MonoBehaviour
+{
+    // Start is called before the first frame update
+    
+    private Camera myCamera;
+    private float _cameraScale = 5;
+    private float cameraScale{get{return _cameraScale;}set{if(value > (float)3f && value < (float)10f){_cameraScale = value;Debug.Log(value);}}}
+    void Start()
+    {
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        if(Input.GetAxis("Mouse ScrollWheel") > 0 )
+        {
+            myCamera = GetComponent<Camera>();
+            cameraScale-=0.5f;
+            myCamera.orthographicSize = cameraScale; 
+        }
+        else if(Input.GetAxis("Mouse ScrollWheel") < 0)
+        {
+            myCamera = GetComponent<Camera>();
+            cameraScale+=0.5f;
+            myCamera.orthographicSize = cameraScale;
+        }
+        
+    }
+
+    
+
+}
---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,67 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+
+    public float WalkSpeed;
+    public float RunSpeed;
+
+    private float Speed;
+    public float SpeedMulti;
+    private Rigidbody2D myRigidBody;
+    private Vector3 change;
+    private Animator animator;
+
+    void Start()
+    {
+        animator = GetComponent<Animator>();
+        myRigidBody = GetComponent<Rigidbody2D>(); // locate component of Rigidbody
+    }
+
+    void Update()
+    {
+        change = Vector3.zero;
+        change.x = Input.GetAxisRaw("Horizontal"); // set user input to x 
+        change.y = Input.GetAxisRaw("Vertical"); // set user input to y
+
+        Debug.Log(change); // output message in console
+        if (change != Vector3.zero) // if change value more not 0 zen call move function, for optimization
+        {
+            if (Input.GetKeyDown(KeyCode.LeftShift))
+            {
+                Speed = RunSpeed;
+            }
+            else
+            {
+                Speed = WalkSpeed;
+            }
+
+            MoveCharacter();
+            animator.SetFloat("moveX", change.x);
+            animator.SetFloat("moveY", change.y);
+            animator.SetBool("moving", true);
+            UpdateAnimation();
+
+        }
+        else
+        {
+            animator.SetBool("moving", false);
+        }
+    }
+
+    void UpdateAnimation()
+    {
+
+    }
+
+    void MoveCharacter()
+    {
+        myRigidBody.MovePosition
+        (
+            transform.position + change * Speed * SpeedMulti * Time.deltaTime
+        );
+    }
+
+}
