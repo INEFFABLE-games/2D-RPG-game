6a4cfd7d3b90b0fe8447e9e0ef20ff547578a454
FAKOTO <ZetoX3429@mail.ru>
2021-06-08 18:46:26 +0300

    FAQ Button

---
+++ Assets/Scripts/StartGame.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class StartGame : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    public void Start_Game()
+    {
+        SceneManager.UnloadSceneAsync("MainMenu");
+        SceneManager.LoadSceneAsync("MainWorld", LoadSceneMode.Single);
+
+
+
+        SceneManager.SetActiveScene(SceneManager.GetSceneByName("MainWorld"));
+
+    }
+
+}
